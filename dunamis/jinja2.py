ba865f90809ea04b5ad7f0c6ffe26db16491a020
Kensuke Kosaka <k@trileg.net>
2016-07-18 00:26:21 +0900

    Modify jinja2.py

--- dunamis/jinja2.py
+++ dunamis/jinja2.py
@@ -7,7 +7,7 @@ from jinja2 import Environment
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
-        'static': staticfiles_storage,
+        'static': staticfiles_storage.url,
         'url': reverse,
     })
     return env
